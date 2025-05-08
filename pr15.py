f43372f0448df8fd87dd53d68fafd98c8cabd95c
Dhasashok <146851861+Dhasashok@users.noreply.github.com>
2025-05-08 14:37:48 +0530

    pr15.py

--- pr15.py
+++ pr15.py
@@ -1,10 +1,9 @@
 def linear_search(arr, target):
     for i in range(len(arr)):
         if arr[i] == target:
-            return i  # Return the index of the target if found
-    return -1  # Return -1 if the target is not found
+            return i  
+    return -1  
 
-# Example usage
 arr = [5, 3, 7, 9, 2]
 target = 7
 result = linear_search(arr, target)
